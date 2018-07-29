aac31d80f08e1f25b57e9df7e12e113f4a6f9483
teranao <terapy822@gmail.com>
2018-07-29 12:39:24 +0900

    changed port defined by heroku

--- app_kitchen.py
+++ app_kitchen.py
@@ -401,11 +401,12 @@ if __name__ == "__main__":
     arg_parser = ArgumentParser(
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
-    arg_parser.add_argument('-p', '--port', type=int, default=8080, help='port')
+    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
     options = arg_parser.parse_args()
 
     # create tmp dir for download content
     make_static_tmp_dir()
 
-    app.run(debug=options.debug, port=options.port)
+    # app.run(debug=options.debug, port=options.port)
+    app.run(debug=options.debug, port=int(os.environ.get("PORT", 5000)))
