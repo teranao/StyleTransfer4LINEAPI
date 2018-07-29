a7b4fea5493ff9e21fff54fb16835b3e1747e3d6
teranao <terapy822@gmail.com>
2018-07-29 12:43:48 +0900

    changed debug on

--- app_kitchen.py
+++ app_kitchen.py
@@ -402,7 +402,7 @@ if __name__ == "__main__":
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
     arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    arg_parser.add_argument('-d', '--debug', default=True, help='debug')
     options = arg_parser.parse_args()
 
     # create tmp dir for download content
